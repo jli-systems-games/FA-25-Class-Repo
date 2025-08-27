a397f2b0fd5efd48779868d3ba971f225b3a354c
mhosster <mmhospe@yahoo.com>
2025-08-27 18:12:11 -0400

    Added input system demo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputActionAsset inputActions;
+
+    private InputAction _action_move;
+    private InputAction _action_jump;
+
+    private void OnEnable()
+    {
+        inputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Player").Disable();
+    }
+
+    void Start()
+    {
+        _action_move = InputSystem.actions.FindAction("Move");
+        _action_jump = InputSystem.actions.FindAction("Jump");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Movement: " + _action_move.ReadValue<Vector2>());
+        Vector2 move = _action_move.ReadValue<Vector2>();
+
+        if (_action_jump.IsPressed())
+            Debug.Log("Jump!");
+    }
+}
