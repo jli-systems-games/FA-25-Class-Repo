9c278144557158c0e87ac2b954df6b8b7c6e552b
liaimv <liaimv0301@gmail.com>
2025-09-02 18:34:26 -0400

    Add ball changes

--- Assets/Assignment 1 - Weird Pong/Scripts/Ball.cs
+++ Assets/Assignment 1 - Weird Pong/Scripts/Ball.cs
@@ -7,7 +7,22 @@ public class Ball : MonoBehaviour
     public Rigidbody2D ballrb;
     public float ballSpeed = 10f;
     public float maxAngle = 0.5f;
+    public float minAngle = 0.2f;
     public float speedMultiplier = 1.1f;
+    [Space(10)]
+
+    //Ball Sprites
+    public Sprite pingSprite;
+    public Sprite tennisSprite;
+    public Sprite basketSprite;
+    public Sprite bowlSprite;
+    public Sprite rugbySprite;
+    public Sprite shuttleSprite;
+    public Sprite beachSprite;
+    public Sprite poolSprite;
+
+    private bool hasChangedSprite = false;
+    [Space(10)]
 
     //Score
     public TextMeshProUGUI leftScoreText;
@@ -18,6 +33,7 @@ public class Ball : MonoBehaviour
     private int rightScore = 0;
     private bool rightWon = false;
     private bool leftWon = false;
+    [Space(10)]
 
     //Game Over
     public Canvas gameOverLeftCanvas;
@@ -58,6 +74,7 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Earn points when ball leaves on the sides
         if (collision.CompareTag("Left"))
         {
             rightScore += 1;
@@ -78,10 +95,115 @@ public class Ball : MonoBehaviour
 
             CheckScores();
         }
+
+        //Ball randomly changes when it reaches the middle line
+        if (collision.CompareTag("Middle Line"))
+        {
+            int randomBall = Random.Range(0, 9);
+
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+            string ballName;
+
+            if (!hasChangedSprite) //Make sure ball doesn't change numerous times when it's on the middle line for serveal frames
+            {
+                if (randomBall == 0) //Bowling Ball
+                {
+                    ballName = "Bowling";
+
+                    ballSpeed = 3;
+                    spriteRenderer.sprite = bowlSprite;
+                    transform.localScale = Vector3.one * 0.044f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 1) //Beach Ball
+                {
+                    ballName = "Beach";
+
+                    ballSpeed = 4;
+                    spriteRenderer.sprite = beachSprite;
+                    transform.localScale = Vector3.one * 0.08f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 2) //Rugby Ball
+                {
+                    ballName = "Rugby";
+
+                    ballSpeed = 5;
+                    spriteRenderer.sprite = rugbySprite;
+                    transform.localScale = Vector3.one * 0.06f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 3) //Pool Ball
+                {
+                    ballName = "Pool";
+
+                    ballSpeed = 6;
+                    spriteRenderer.sprite = poolSprite;
+                    transform.localScale = Vector3.one * 0.0114f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 4) //Basketball
+                {
+                    ballName = "Basket";
+
+                    ballSpeed = 7;
+                    spriteRenderer.sprite = basketSprite;
+                    transform.localScale = Vector3.one * 0.048f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 5) //Shuttlecock
+                {
+                    ballName = "Shuttle";
+
+                    ballSpeed = 9;
+                    spriteRenderer.sprite = shuttleSprite;
+                    transform.localScale = Vector3.one * 0.012f;
+
+                    hasChangedSprite = true;
+                }
+                else if (randomBall == 6) //Tennis Ball
+                {
+                    ballName = "Tennis";
+
+                    ballSpeed = 10;
+                    spriteRenderer.sprite = tennisSprite;
+                    transform.localScale = Vector3.one * 0.0134f;
+
+                    hasChangedSprite = true;
+                }
+                else //Ping Pong Ball
+                {
+                    ballName = "Ping";
+
+                    ballSpeed = 12;
+                    spriteRenderer.sprite = pingSprite;
+                    transform.localScale = Vector3.one * 0.008f;
+
+                    hasChangedSprite = true;
+                }
+
+                Debug.Log(ballSpeed + ballName);
+
+                Vector2 currentDirection = ballrb.linearVelocity.normalized;
+                ballrb.linearVelocity = currentDirection * ballSpeed;
+            }
+        }
+
+        if (collision.CompareTag("Exit Boundary"))
+        {
+            hasChangedSprite = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Ball speed increases everytime it hits the paddle
         if (collision.gameObject.CompareTag("Paddle"))
         {
             ballrb.linearVelocity *= speedMultiplier;
@@ -123,14 +245,37 @@ public class Ball : MonoBehaviour
     void StartLeft()
     {
         Vector2 ballDirection = Vector2.left;
-        ballDirection.y = Random.Range(-maxAngle, maxAngle);
+
+        int randomHeight = Random.Range(0, 2);
+
+        //Make sure ball starts off at a reasonable random angle (not straight either)
+        if (randomHeight == 0)
+        {
+            ballDirection.y = Random.Range(-minAngle, -maxAngle);
+        }
+        else
+        {
+            ballDirection.y = Random.Range(maxAngle, minAngle);
+        }
+
         ballrb.linearVelocity = ballDirection * ballSpeed;
     }
 
     void StartRight()
     {
         Vector2 ballDirection = Vector2.right;
-        ballDirection.y = Random.Range(-maxAngle, maxAngle);
+
+        int randomHeight = Random.Range(0, 2);
+
+        if (randomHeight == 0)
+        {
+            ballDirection.y = Random.Range(-minAngle, -maxAngle);
+        }
+        else
+        {
+            ballDirection.y = Random.Range(maxAngle, minAngle);
+        }
+
         ballrb.linearVelocity = ballDirection * ballSpeed;
     }
 
--- Assets/Assignment 1 - Weird Pong/Scripts/PaddleControl.cs
+++ Assets/Assignment 1 - Weird Pong/Scripts/PaddleControl.cs
@@ -11,6 +11,7 @@ public class PaddleControl : MonoBehaviour
     
     void OnMovement(InputValue value)
     {
+        //Get input from input action
         moveInput = value.Get<Vector2>();
     }
 
@@ -18,6 +19,7 @@ public class PaddleControl : MonoBehaviour
     {
         Vector2 paddlePos = transform.position;
 
+        //Make sure paddles go back to original position when game over
         if (ball.hasGameOver)
         {
             paddlePos.y = 0;
@@ -25,6 +27,7 @@ public class PaddleControl : MonoBehaviour
         }
         else 
         {
+            //Move according to input action controls
             paddlePos.y += moveInput.y * paddleSpeed * Time.deltaTime;
             paddlePos.y = Mathf.Clamp(paddlePos.y, -verticalLimit, verticalLimit); //Clamp so that the paddle doesn't go out of bounds
             transform.position = paddlePos;
