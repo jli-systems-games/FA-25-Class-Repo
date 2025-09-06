00475f313dce2e6313eb226916ea64e83ee90bcb
liaimv <liaimv0301@gmail.com>
2025-09-06 15:08:21 -0400

    Code for spawning nails

--- Assets/Assignment 2 - Warioware/Scripts/MalletControl.cs
+++ Assets/Assignment 2 - Warioware/Scripts/MalletControl.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
-public class HammerControl : MonoBehaviour
+public class MalletControl : MonoBehaviour
 {
     private Animator malletAnimator;
     public bool mouseClicked;
     [Space(10)]
 
     public float nailDownAmount = 0.05f;
-    private float shortNailFinalPosition = 0.0038f;
-    private float longNailFinalPosition = -0.031f;
+    private float nailFinalPosition = -0.031f;
 
     void Start()
     {
@@ -16,9 +15,10 @@ public class HammerControl : MonoBehaviour
     }
     void Update()
     {
+        //Make the mallet move according to mouse position
         Vector3 mousePos = Input.mousePosition;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 1)); //Code from https://discussions.unity.com/t/3d-object-follow-mouse/792952
-        transform.position = new Vector3(worldPos.x, 0.3064f, worldPos.z);
+        transform.position = new Vector3(worldPos.x, 0.3064f, worldPos.z); 
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -31,16 +31,10 @@ public class HammerControl : MonoBehaviour
     {
         if (mouseClicked)
         {
+            //Make nails go down when mallet hits it
             Vector3 newPos = other.transform.position;
             newPos.y -= nailDownAmount;
-            if (other.gameObject.CompareTag("Short Nail"))
-            {
-                newPos.y = Mathf.Max(newPos.y, shortNailFinalPosition);
-            }
-            else if (other.gameObject.CompareTag("Long Nail"))
-            {
-                newPos.y = Mathf.Max(newPos.y, longNailFinalPosition);
-            }
+            newPos.y = Mathf.Max(newPos.y, nailFinalPosition);
 
             other.transform.position = newPos;
 
---
+++ Assets/Assignment 2 - Warioware/Scripts/NailSpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class NailSpawner : MonoBehaviour
+{
+    public GameObject nailPrefab;
+    public int nailCount = 10;
+    public float minRadius = 0.1f;
+    public LayerMask nailLayer;
+    [Space(10)]
+
+    //Spawn Area Bounds
+    public float spawnAreaXMax = -6.4f;
+    public float spawnAreaXMin = -7.2f;
+    public float spawnAreaYMax = 0.128f;
+    public float spawnAreaYMin = 0.056f;
+    public float spawnAreaZMax = -1.57f;
+    public float spawnAreaZMin = -1.97f;
+
+    void Start()
+    {
+        SpawnNails();
+    }
+
+    void SpawnNails()
+    {
+        int spawned = 0;
+
+        while (spawned < nailCount) //keep looping the spawn so that it will  instantiate until it finds a right position for the nails
+        {
+            Vector3 randomPos = new Vector3(
+                Random.Range(spawnAreaXMin, spawnAreaXMax),
+                Random.Range(spawnAreaYMin, spawnAreaYMax),
+                Random.Range(spawnAreaZMin, spawnAreaZMax)
+            );
+
+            if (!Physics.CheckSphere(randomPos, minRadius, nailLayer)) //Check Sphere: Creates a sphere and checks if it collides with any object in that layer
+            {
+                GameObject newNail = Instantiate(nailPrefab, randomPos, Quaternion.Euler(90f, 0f, 0f), transform);
+                newNail.transform.localScale = Vector3.one * 17.664f;
+                newNail.layer = LayerMask.NameToLayer("Nail");
+                spawned++;
+            }
+        }
+        //Got code to check overlapping objects from https://www.youtube.com/watch?v=ENEtzLePZbQ&ab_channel=Rabidgremlin
+    }
+}
