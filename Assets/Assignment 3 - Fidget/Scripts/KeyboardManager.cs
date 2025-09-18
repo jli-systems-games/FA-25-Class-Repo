982ee398eff02b5020a11f91b9134529486ebd60
liaimv <liaimv0301@gmail.com>
2025-09-17 21:23:03 -0400

    Add keyboard and keyboard manager script

---
+++ Assets/Assignment 3 - Fidget/Scripts/KeyboardManager.cs
@@ -0,0 +1,63 @@
+using System.Security.Cryptography;
+using UnityEngine;
+using System.Collections;
+using static UnityEditor.PlayerSettings;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public Material keyboardWhite;
+    public Material keyboardBlack;
+
+    private Vector3 OGpos;
+    private float keyDownAmount = 0.0024f;
+    private bool isDown = false;
+
+    void Start()
+    {
+        int randomColor = Random.Range(0, 2);
+
+        if (randomColor == 0)
+        {
+            gameObject.GetComponent<Renderer>().material = keyboardBlack;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material = keyboardWhite;
+        }
+
+        OGpos = transform.localPosition;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isDown)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.z -= keyDownAmount;
+            transform.localPosition = newPos;
+
+            isDown = true;
+
+            Debug.Log("Player hit keyboard");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(DelayBeforeReturningKey(0.5f));
+        }
+    }
+
+    private IEnumerator DelayBeforeReturningKey(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        transform.localPosition = OGpos;
+
+        isDown = false;
+
+        Debug.Log("Player left keyboard");
+    }
+}
