f293f827e4b72f3cacf1287169bffbc73be36236
NikkiTian <tianmiaomiao994@gmail.com>
2025-09-22 18:41:37 -0400

    base game built

---
+++ Assets/FirstPersonMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private float sprintSpeed;
+    public float lookSpeed = 2f;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    //public CharacterController fly;
+    //private float xRot;
+
+    void Start()
+    {
+        sprintSpeed = moveSpeed * 5f;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if ((Input.GetKey(KeyCode.LeftArrow)|| Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.D)))
+        {
+            moveDirection = Camera.main.transform.forward;
+            if(Input.GetKey(KeyCode.Space))
+            {
+                controller.Move(moveDirection * sprintSpeed * Time.deltaTime);
+            }
+            else controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+
+        }
+
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.Rotate(Vector3.left * mouseY);
+    }
+}
