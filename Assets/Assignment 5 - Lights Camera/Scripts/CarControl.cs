197dee3729aed585470eb6b373d5d0038332b6cd
liaimv <liaimv0301@gmail.com>
2025-09-27 16:54:43 -0400

    Add car control script

---
+++ Assets/Assignment 5 - Lights Camera/Scripts/CarControl.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    public float forwardForce = 500f;
+    public float extraGravity = 30f;
+    public float forwardAngle = 2f;
+
+    private Rigidbody rb;
+    private bool inBuilding = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Building"))
+        {
+            inBuilding = false;
+        }    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && inBuilding)
+        {
+            rb.AddForce(transform.forward * forwardForce, ForceMode.Impulse);
+
+            Debug.Log("Force pushed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("No more speed.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!inBuilding)
+        {
+            rb.AddForce(Vector3.down * extraGravity, ForceMode.Acceleration);
+
+            rb.AddTorque(Vector3.right * forwardAngle, ForceMode.Acceleration);
+        }
+    }
+}
