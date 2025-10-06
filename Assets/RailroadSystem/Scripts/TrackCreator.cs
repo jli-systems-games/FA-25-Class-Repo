19716f77a1bf95325a63122cf5d68be628b8084a
Dmanyan <yanlongren1106@gmail.com>
2025-10-06 16:09:50 -0400

    train?

---
+++ Assets/RailroadSystem/Scripts/TrackCreator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Unity.Cinemachine; // new namespace!
+using UnityEngine.Splines;
+
+public class TrackCreator : MonoBehaviour
+{
+    [SerializeField] SplineContainer track;
+    [SerializeField] bool loopedTrack = false;
+
+    private Spline spline;
+
+    void Start()
+    {
+        GenerateTrack();
+    }
+
+    public void GenerateTrack()
+    {
+        if (!track)
+        {
+            Debug.LogError("No spline track assigned.");
+            return;
+        }
+
+        spline = track.Spline;
+        spline.Closed = loopedTrack;
+
+        // Example: clear old points and add new ones from child transforms
+        spline.Clear();
+
+        foreach (Transform child in track.transform)
+        {
+            spline.Add(new BezierKnot(child.localPosition));
+        }
+
+        // You can use CinemachineSplineDolly or CinemachineSplineCart to follow this spline
+        Debug.Log($"Generated spline with {spline.Count} waypoints. Looped: {loopedTrack}");
+    }
+}
