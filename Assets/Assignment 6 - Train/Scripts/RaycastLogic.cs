d4bf36ad2fec31d3f432f7a1e03ceacc7c28fe04
liaimv <liaimv0301@gmail.com>
2025-10-08 17:26:16 -0400

    Add raycast logic

--- Assets/Assignment 6 - Train/Scripts/RaycastLogic.cs
+++ Assets/Assignment 6 - Train/Scripts/RaycastLogic.cs
@@ -1,16 +1,56 @@
+using TMPro;
 using UnityEngine;
 
 public class RaycastLogic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Camera playerCamera;
+    public float interactionDistance = 3f;
+    public Canvas interactionCanvas;
+    public Canvas ticketCanvas;
+    public LayerMask interactableLayer;
+
     void Start()
     {
-        
+        interactionCanvas.gameObject.SetActive(false);
+        ticketCanvas.gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * interactionDistance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, interactionDistance, interactableLayer))
+        {
+            if (hit.collider.CompareTag("Passenger"))
+            {
+                Debug.Log("Found passenger");
+                interactionCanvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                interactionCanvas.gameObject.SetActive(false);
+            }
+        }
+
+        if (interactionCanvas.isActiveAndEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ticketCanvas.gameObject.SetActive(true);
+            }
+        }
+
+        if (ticketCanvas.isActiveAndEnabled)
+        {
+            interactionCanvas.gameObject.SetActive(false);
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ticketCanvas.gameObject.SetActive(false);
+            }
+        }
     }
 }
---
+++ Assets/Assignment 6 - Train/Scripts/TicketManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TicketManager : MonoBehaviour
+{
+    private GameObject currentPassenger;
+
+    void Start()
+    {
+        currentPassenger = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
