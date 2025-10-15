139657ef44585d72ef37f743482c08b751391baf
hyunju12345 <hyunju03yoo@gmail.com>
2025-10-15 16:13:51 -0400

    ????

--- Assets/week 7 game/script/CarCustomizerManager.cs
+++ Assets/week 7 game/script/CarCustomizerManager.cs
@@ -129,6 +129,9 @@ public class CarCustomizerManager : MonoBehaviour
         PlayerPrefs.SetFloat("car_color_b", savedColor.b);
         PlayerPrefs.SetFloat("car_color_a", savedColor.a);
         PlayerPrefs.Save();
+<<<<<<< HEAD
+>>>>>>> parent of 5ec2ef95 (final racing input)
+=======
 >>>>>>> parent of 5ec2ef95 (final racing input)
     }
 }
