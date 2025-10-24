db25f8f9c63e1c202479648c90dc0feb9949ac53
liaimv <liaimv0301@gmail.com>
2025-10-24 12:11:16 -0400

    Add cleaning logic

--- Assets/Assignment 8 - Pet Life/Scripts/CleanSlime.cs
+++ Assets/Assignment 8 - Pet Life/Scripts/CleanSlime.cs
@@ -1,16 +1,88 @@
+using System.Collections;
 using UnityEngine;
 
 public class CleanSlime : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Material dirtMaterial;
+
+    public LayerMask slimeDirtLayer;
+
+    private Vector2 currentPos;
+    private Vector2 lastPos;
+    private bool isMouseMoving = false;
+
+    private void Start()
     {
-        
+        lastPos = Input.mousePosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Data.isCleanMode)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Mouse down");
+
+                if (RaycastHitSlime())
+                {
+                    Debug.Log("Raycast hit slime");
+
+                    currentPos = Input.mousePosition;
+
+                    if (currentPos != lastPos)
+                    {
+                        isMouseMoving = true;
+                    }
+                    else
+                    {
+                        isMouseMoving = false;
+                    }
+
+                    if (isMouseMoving)
+                    {
+                        Debug.Log("clean swipe occured");
+                        CleanSlimeFlick();
+                    }
+
+                    lastPos = currentPos;
+                }
+            }
+
+            UpdateDirtinessVisual();
+        }
+    }
+
+    private bool RaycastHitSlime()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, slimeDirtLayer))
+        {
+            return hit.transform == transform;
+        }
+        return false;
+    }
+
+    private void CleanSlimeFlick()
+    {
+        Data.dirtStat += 0.1f;
+
+        if (Data.dirtStat > 100)
+        {
+            Data.dirtStat = 100;
+        }
+
+        Debug.Log(Data.dirtStat);
+    }
+
+    private void UpdateDirtinessVisual()
+    {
+        Color dirtColor = dirtMaterial.color;
+
+        dirtColor.a = 1 - Data.dirtStat / 100f;
+
+        dirtMaterial.color = dirtColor;
     }
 }
---
+++ Assets/Assignment 8 - Pet Life/Scripts/Data.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Data
+{
+    public static float dirtStat = 0;
+    public static float hungerStat;
+    public static float cleanStat;
+
+    public static bool isDirtMode;
+    public static bool isHungerMode;
+    public static bool isCleanMode = true;
+}
