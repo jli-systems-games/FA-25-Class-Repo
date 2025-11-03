11e1b9e75755f32a60250b0a92af5e412c677349
liaimv <liaimv0301@gmail.com>
2025-11-03 18:42:51 -0500

    Add hallway instance and sphere roll logic

--- Assets/Assignment 10 - Just Add World/Scripts/HallwayInstance.cs
+++ Assets/Assignment 10 - Just Add World/Scripts/HallwayInstance.cs
@@ -1,16 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HallwayInstance : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject hallwayPrefab;
+    public float hallwayLength = 10;
+    public int numSections = 5;
+    public Transform player;
+
+    private List<GameObject> hallways =
+        new List<GameObject>();
+
+    private float lastSpawnZ;
+
     void Start()
     {
-        
+        for (int i = 0; i < numSections; i++)
+        {
+            Vector3 position = new Vector3(0, 0, i * hallwayLength);
+            GameObject section = Instantiate(hallwayPrefab, position, Quaternion.identity);
+            hallways.Add(section);
+            lastSpawnZ = position.z;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (player.position.z + (hallwayLength * 2) > lastSpawnZ)
+        {
+            SpawnSection();
+        }
+    }
+
+    void SpawnSection()
+    {
+        lastSpawnZ += hallwayLength;
+        Vector3 position = new Vector3(0, 0, lastSpawnZ);
+        GameObject section = Instantiate(hallwayPrefab, position, Quaternion.identity);
+        hallways.Add(section);
     }
 }
---
+++ Assets/Assignment 10 - Just Add World/Scripts/SphereRoll.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SphereRoll : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rollSpeed;
+
+    void Update()
+    {
+        //Movement
+        Vector3 move = Vector3.forward * moveSpeed * Time.deltaTime;
+        transform.position += move;
+
+        //Rotation
+        transform.Rotate(Vector3.right, rollSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hallway"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
