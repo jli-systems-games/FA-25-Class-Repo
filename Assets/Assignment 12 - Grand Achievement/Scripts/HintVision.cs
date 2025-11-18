b1224004d4d6a939f43edeb8cb2250021dcf3d9d
liaimv <liaimv0301@gmail.com>
2025-11-17 19:57:49 -0500

    Add animal statues and glow fade logic

--- Assets/Assignment 12 - Grand Achievement/Scripts/HintVision.cs
+++ Assets/Assignment 12 - Grand Achievement/Scripts/HintVision.cs
@@ -6,6 +6,7 @@ public class HintVision : MonoBehaviour
 {
     public Camera hintCamera;
     public Volume hintVisionVolume;
+    public Volume hintCameraVolume;
     public float hintVisionDuration = 2f;
     public float fadeDuration = 1f;
     public float coolDownTime = 5f;
@@ -13,10 +14,16 @@ public class HintVision : MonoBehaviour
     private bool isOnCooldown = false;
     private Coroutine currentCoroutine;
 
+    private Color OGEmissionColor = new Color(0.0f, 0.749f, 0.749f, 0f);
+
+    //Materials
+    public Material mossyRockHintMaterial;
+
     void Start()
     {
         hintCamera.enabled = false;
         hintVisionVolume.weight = 0;
+        hintCameraVolume.weight = 0;
     }
 
     void Update()
@@ -34,11 +41,13 @@ public class HintVision : MonoBehaviour
 
     private IEnumerator HintVisionSequence()
     {
-        yield return StartCoroutine(HintVisionFade(1f));
         hintCamera.enabled = true;
+        yield return StartCoroutine(HintVisionFade(1f));
+        yield return StartCoroutine(HintGlowEmissionColorFade(mossyRockHintMaterial, OGEmissionColor));
 
         yield return new WaitForSeconds(hintVisionDuration);
 
+        yield return StartCoroutine(HintGlowEmissionColorFade(mossyRockHintMaterial, Color.black));
         yield return StartCoroutine(HintVisionFade(0f));
         hintCamera.enabled = false;
 
@@ -55,9 +64,27 @@ public class HintVision : MonoBehaviour
         {
             t += Time.deltaTime;
             hintVisionVolume.weight = Mathf.Lerp(startWeight, weightTarget, t / fadeDuration);
+            hintCameraVolume.weight = Mathf.Lerp(startWeight, weightTarget, t / fadeDuration);
             yield return null;
         }
 
         hintVisionVolume.weight = weightTarget;
+        hintCameraVolume.weight = weightTarget;
+    }
+
+    private IEnumerator HintGlowEmissionColorFade(Material material, Color emissionColor)
+    {
+        Color startEmission = material.GetColor("_GlowEmission");
+        Color emissionTarget = emissionColor;
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            material.SetColor("_GlowEmission", Color.Lerp(startEmission, emissionTarget, t / fadeDuration));
+            yield return null;
+        }
+
+        material.SetColor("_GlowEmission", emissionTarget);
     }
 }
