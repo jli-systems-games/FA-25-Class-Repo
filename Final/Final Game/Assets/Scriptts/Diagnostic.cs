e91d938e9a201899104222cf4300ff341dc230b7
ba-na-na121 <xiaw902@newschool.edu>
2025-12-11 02:21:36 -0500

    FinalFinal

--- Final/Final Game/Assets/Scriptts/Diagnostic.cs
+++ Final/Final Game/Assets/Scriptts/Diagnostic.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using UnityEditor;
 
 /// <summary>
@@ -178,4 +178,4 @@ public class InputDiagnosticsSetup : EditorWindow
                 "确定");
         }
     }
-}
+}*/
